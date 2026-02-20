db187ba20305da59b87c42d6b93176aaa44794ce
khoiw04 <khoi.w04@gmail.com>
2026-02-20 09:32:06 +0700

    Remove /mail/ out of Google Search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,69 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare'
-import react from '@astrojs/react';
-import playformInline from '@playform/inline'
-import remarkMath from 'remark-math'
-import remarkDirective from 'remark-directive'
-import rehypeKatex from 'rehype-katex'
-import rehypeCleanup from './src/plugins/rehype-cleanup.mjs'
-import remarkEmbeddedMedia from './src/plugins/remark-embedded-media.mjs'
-import rehypeAddButtonClass from './src/plugins/rehype-add-button-class.js';
-import rehypeImageProcessor from './src/plugins/rehype-image-processor.mjs'
-import rehypeAstroRelativeLinks from './src/plugins/rehype-astro-relative-links.js';
-import remarkAdmonitions from './src/plugins/remark-admonitions.mjs'
-import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import playformInline from "@playform/inline";
+import remarkMath from "remark-math";
+import remarkDirective from "remark-directive";
+import rehypeKatex from "rehype-katex";
+import rehypeCleanup from "./src/plugins/rehype-cleanup.mjs";
+import remarkEmbeddedMedia from "./src/plugins/remark-embedded-media.mjs";
+import rehypeAddButtonClass from "./src/plugins/rehype-add-button-class.js";
+import rehypeImageProcessor from "./src/plugins/rehype-image-processor.mjs";
+import rehypeAstroRelativeLinks from "./src/plugins/rehype-astro-relative-links.js";
+import remarkAdmonitions from "./src/plugins/remark-admonitions.mjs";
+import tailwindcss from "@tailwindcss/vite";
 
-import expressiveCode from 'astro-expressive-code';
-import { imageConfig } from './src/lib/image-config.js';
-import { themeConfig } from './src/config.js';
+import expressiveCode from "astro-expressive-code";
+import { imageConfig } from "./src/lib/image-config.js";
+import { themeConfig } from "./src/config.js";
 
 const plugins = {
-    remarkPlugins: [remarkDirective, remarkAdmonitions, remarkEmbeddedMedia, remarkMath],
-    rehypePlugins: [rehypeKatex, rehypeCleanup, rehypeImageProcessor, rehypeAddButtonClass, rehypeAstroRelativeLinks]
-}
+  remarkPlugins: [
+    remarkDirective,
+    remarkAdmonitions,
+    remarkEmbeddedMedia,
+    remarkMath,
+  ],
+  rehypePlugins: [
+    rehypeKatex,
+    rehypeCleanup,
+    rehypeImageProcessor,
+    rehypeAddButtonClass,
+    rehypeAstroRelativeLinks,
+  ],
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: themeConfig.site.website,
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
-      wrap: false
+      theme: "css-variables",
+      wrap: false,
     },
-    ...plugins
+    ...plugins,
   },
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp',
-      config: imageConfig
+      entrypoint: "astro/assets/services/sharp",
+      config: imageConfig,
     },
   },
   integrations: [
     playformInline({
-      Exclude: [(file) => file.toLowerCase().includes('katex')]
+      Exclude: [(file) => file.toLowerCase().includes("katex")],
     }),
     expressiveCode(),
     mdx({
-      ...plugins
+      ...plugins,
     }),
-    sitemap(),
+    sitemap({
+      filter: (page) => !page.includes("/mail/"),
+    }),
     react(),
     (await import("astro-compress")).default(),
   ],
@@ -61,8 +74,8 @@ export default defineConfig({
     locales: ["vi", "en"],
     defaultLocale: "vi",
   },
-  output: 'server',
+  output: "server",
   adapter: cloudflare({
-    imageService: "compile"
+    imageService: "compile",
   }),
 });
