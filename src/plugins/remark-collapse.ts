b76886a90778cfd361c3d8c83a2457d06ba04f25
khoiw04 <khoi.w04@gmail.com>
2026-04-16 19:24:15 +0700

    Add "open" init

--- src/plugins/remark-collapse.ts
+++ src/plugins/remark-collapse.ts
@@ -7,39 +7,34 @@ export default function remarkCollapse() {
     visit(tree, "containerDirective", (node: ContainerDirective) => {
       if (node.name !== "collapse") return;
 
-      const data = node.data || (node.data = {});
+      const data = (node.data ??= {});
+
+      const isOpen = node.attributes?.open === "true";
+
       data.hName = "details";
 
-      let title = "Xem chi tiết";
-
-      const firstChild = node.children[0] as any;
-
-      if (firstChild?.data?.directiveLabel) {
-        title = firstChild.children[0]?.value || title;
-        node.children.shift();
-      } else if (node.attributes?.title) {
-        title = node.attributes.title;
-      }
-
-      const tag = node.attributes?.as;
-
-      let titleNode: any = { type: "text" as const, value: title };
-
-      if (tag && ["h1", "h2", "h3", "h4"].includes(tag)) {
-        titleNode = {
-          type: "heading" as const,
-          depth: parseInt(tag.charAt(1)),
-          children: [{ type: "text" as const, value: title }],
-        };
-      }
-
-      const summaryNode = {
-        type: "paragraph" as const,
-        data: { hName: "summary" },
-        children: [titleNode],
+      data.hProperties = {
+        ...(data.hProperties || {}),
+        open: isOpen ? true : undefined,
       };
 
-      node.children.unshift(summaryNode as any);
+      // title
+      let title = node.attributes?.title || "Xem chi tiết";
+
+      const firstChild = node.children?.[0] as any;
+
+      if (firstChild?.data?.directiveLabel) {
+        title = firstChild.children?.[0]?.value || title;
+        node.children.shift();
+      }
+
+      const summary = {
+        type: "paragraph",
+        data: { hName: "summary" },
+        children: [{ type: "text", value: title }],
+      };
+
+      node.children.unshift(summary as any);
     });
   };
 }
