f97d729a7f4cd43e86cdb778cc8227662541d7d2
khoiw04 <khoi.w04@gmail.com>
2026-04-25 21:25:55 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -114,7 +114,6 @@ export default defineConfig({
       RESEND_API_KEY: envField.string({
         context: "server",
         access: "secret",
-        optional: true,
       }),
       NEWSLETTER_KHOI_BLOG_EN: envField.string({
         context: "server",
@@ -139,7 +138,6 @@ export default defineConfig({
       TURNSTILE_SECRETKEY: envField.string({
         context: "server",
         access: "secret",
-        optional: true,
       }),
       WALINE_SITE_URL: envField.string({
         context: "server",
