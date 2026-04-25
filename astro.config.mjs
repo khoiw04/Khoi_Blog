85d8001832aca05439b209565230110c33fa7b5a
khoiw04 <khoi.w04@gmail.com>
2026-04-25 21:03:11 +0700

    Add optional to RESEND_API_KEY

--- astro.config.mjs
+++ astro.config.mjs
@@ -114,6 +114,7 @@ export default defineConfig({
       RESEND_API_KEY: envField.string({
         context: "server",
         access: "secret",
+        optional: true,
       }),
       NEWSLETTER_KHOI_BLOG_EN: envField.string({
         context: "server",
